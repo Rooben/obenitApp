653f2d0eb36d1e28faa16256d9221a886238ee35
Roland Oben <rooben@rogers.com>
2015-05-16 22:21:31 -0400

    Added the controller for the skills page.

---
+++ app/scripts/controllers/skills.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rolandApp.controller:SkillsCtrl
+ * @description
+ * # SkillsCtrl
+ * Controller of the rolandApp
+ */
+angular.module('rolandApp')
+  .controller('SkillsCtrl', function ($scope) {
+
+  });
+
+
